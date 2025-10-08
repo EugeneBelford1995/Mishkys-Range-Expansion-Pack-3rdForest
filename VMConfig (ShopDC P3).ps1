2c9d01712d53643dc5b98af0c55d2470d8132281
Rich <114684848+EugeneBelford1995@users.noreply.github.com>
2025-10-08 12:26:44 -0700

    Update VMConfig (ShopDC P3).ps1

--- VMConfig (ShopDC P3).ps1
+++ VMConfig (ShopDC P3).ps1
@@ -32,5 +32,21 @@ New-ADUser -SamAccountName $OtherUser -Name $OtherUser -UserPrincipalName "$Othe
 Add-ADGroupMember -Identity "Server Admins" -Members "Dan" #This group will have WriteProperty Membership Property Set GUID over the group that's denied GPO rights
 
 #Put Shop-Client in two groups; one is allowed GenericAll on a GPO (Servers) and the other (Staging) is denied GenericAll on the same GPO
-#Dan's group Server Admins will have the right to add/remove users from the denied group (Staging)
-#Ask CW6 Google how to create a GPO and apply it to the Domian Controllers OU. This will be the target GPO
+#Create a GPO and apply it to the Domian Controllers OU. This will be the target GPO. Use the NTP GPO as an example, in fact I could use that one as is.
+
+$victim = (Get-ADObject "<GPO>,$ADRoot" -Properties *).DistinguishedName
+$acl = Get-ACL $victim
+$user = New-Object System.Security.Principal.SecurityIdentifier (Get-ADGroup "Servers").SID
+$user2 = New-Object System.Security.Principal.SecurityIdentifier (Get-ADGroup "Staging").SID
+$acl.AddAccessRule((New-Object System.DirectoryServices.ActiveDirectoryAccessRule $user,"GenericAll","ALLOW",([GUID]("00000000-0000-0000-0000-000000000000")).guid,"None",([GUID]("00000000-0000-0000-0000-000000000000")).guid))
+$acl.AddAccessRule((New-Object System.DirectoryServices.ActiveDirectoryAccessRule $user2,"GenericAll","DENY",([GUID]("00000000-0000-0000-0000-000000000000")).guid,"None",([GUID]("00000000-0000-0000-0000-000000000000")).guid))
+#Apply above ACL rules
+Set-ACL $victim $acl
+
+#Dan's group Server Admins will have the right to add/remove users from the denied group (Staging) via WriteProperty Membership Set
+$victim = (Get-ADGroup "Staging" -Properties *).DistinguishedName
+$acl = Get-ACL $victim
+$user = New-Object System.Security.Principal.SecurityIdentifier (Get-ADGroup "Server Admins").SID
+$acl.AddAccessRule((New-Object System.DirectoryServices.ActiveDirectoryAccessRule $user,"GenericAll","ALLOW",([GUID]("00000000-0000-0000-0000-000000000000")).guid,"None",([GUID]("00000000-0000-0000-0000-000000000000")).guid))
+#Apply above ACL rules
+Set-ACL $victim $acl
